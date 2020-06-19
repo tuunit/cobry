859d2dc47c96a28264b4fae536d1568c76fe4402
Jan Larwig <jan@larwig.com>
2020-06-19 18:36:52 +0200

    update build and install script

--- build_install.sh
+++ build_install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+echo -e "\n====== Activate venv ======\n"
 source venv/bin/activate 
 
 rm -rf dist build cobry.egg-info 
 
+echo -e "\n======= Build corby =======\n"
 python setup.py sdist bdist_wheel
 
-if twine check dist/*; then
-    pip install dist/*.tar.gz
-fi
+echo -e "\n====== Install corby ======\n"
+pip install dist/*.tar.gz
 
+echo -e "\n======== Clean up =========\n"
 rm -rf dist build cobry.egg-info 
